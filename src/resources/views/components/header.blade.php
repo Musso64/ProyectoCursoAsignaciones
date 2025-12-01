86c508da4a14ad59f3cde07af887049b3ca40b7a
Roman Santiago Alvarez Buitrago <roman.alvarez@mazars.com.ve>
2025-12-01 16:59:48 -0400

    Minor Advances regarding login and header component.

--- src/resources/views/components/header.blade.php
+++ src/resources/views/components/header.blade.php
@@ -3,6 +3,16 @@
         <div class="container-fluid">
             <span class="navbar-brand ms-4 me-4 h4 mb-0">L'Magno Audittore</span>
             <ul class="list-inline mb-0 me-4">
+            @guest
+                <li class="list-inline-item">
+                    <a href="{{ route('login') }}" class="text-white px-4 py-3 rounded hover-bg-fade {{ request()->routeIs('login') ? 'active-nav' : '' }}">Inicio de Sesion</a>
+                </li>
+                @if (Route::has('register'))
+                <li class="list-inline-item">
+                    <a href="{{ route('register') }}" class="text-white px-4 py-3 rounded hover-bg-fade {{ request()->routeIs('register') ? 'active-nav' : '' }}"></a>
+                </li>
+                @endif
+            @else
                 <li class="list-inline-item">
                     <a href="{{ route('home') }}" class="text-white px-4 py-3 rounded hover-bg-fade {{ request()->routeIs('home') ? 'active-nav' : '' }}">Inicio</a>
                 </li>
@@ -15,6 +25,23 @@
                 <li class="list-inline-item">
                     <a href="{{ route('assignments.index') }}" class="text-white px-4 py-3 rounded hover-bg-fade {{ request()->is('assignments*') ? 'active-nav' : '' }}">Asignaciones</a>
                 </li>
+                <li class="list-inline-item dropdown">
+                    <a id="navbarDropdown" class="text-white px-4 py-3 rounded hover-bg-fade dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+            @endguest
             </ul>
         </div>
     </nav>
